158ed40410aead7058a0fe020545179caa867394
sergiomor1973 <159595103+sergiomor1973@users.noreply.github.com>
2026-05-18 10:53:52 +0100

    Update ps2toolchain-config.sh

--- config/ps2toolchain-config.sh
+++ config/ps2toolchain-config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-PS2TOOLCHAIN_DVP_REPO_URL="https://github.com/ps2dev/ps2toolchain-dvp.git"
+PS2TOOLCHAIN_DVP_REPO_URL="https://github.com/sergiomor1973/ps2toolchain-dvp.git"
 PS2TOOLCHAIN_DVP_DEFAULT_REPO_REF="main"
-PS2TOOLCHAIN_IOP_REPO_URL="https://github.com/ps2dev/ps2toolchain-iop.git"
+PS2TOOLCHAIN_IOP_REPO_URL="https://github.com/sergiomor1973/ps2toolchain-iop.git"
 PS2TOOLCHAIN_IOP_DEFAULT_REPO_REF="main"
-PS2TOOLCHAIN_EE_REPO_URL="https://github.com/ps2dev/ps2toolchain-ee.git"
+PS2TOOLCHAIN_EE_REPO_URL="https://github.com/sergiomor1973/ps2toolchain-ee.git"
 PS2TOOLCHAIN_EE_DEFAULT_REPO_REF="main"
 
 if test -f "$PS2DEV_CONFIG_OVERRIDE"; then
