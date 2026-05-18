a1b6cd1782582a3d60ae978ace061a03818f84b7
sergiomor1973 <159595103+sergiomor1973@users.noreply.github.com>
2026-05-18 17:44:27 +0100

    Update build-all-full.sh

--- build-all-full.sh
+++ build-all-full.sh
@@ -12,21 +12,21 @@ cd "$(dirname "$0")"
 # 1. Build DVP
 echo "=== [1/3] Building DVP ==="
 cd build/ps2toolchain-dvp
-bash build-all.sh
+bash toolchain.sh
 cd ../..
 
 # 2. Build IOP
 echo ""
 echo "=== [2/3] Building IOP ==="
 cd build/ps2toolchain-iop
-bash build-all.sh
+bash toolchain.sh
 cd ../..
 
 # 3. Build EE
 echo ""
 echo "=== [3/3] Building EE ==="
 cd build/ps2toolchain-ee
-bash build-all.sh
+bash toolchain.sh
 cd ../..
 
 echo ""
