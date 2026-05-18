61253160101b74231ceaf6ecf06b7fa66ad7f753
sergiomor1973 <159595103+sergiomor1973@users.noreply.github.com>
2026-05-18 11:19:38 +0100

    Update toolchain-sudo.sh

--- toolchain-sudo.sh
+++ toolchain-sudo.sh
@@ -10,7 +10,7 @@ export PS2SDK=$PS2DEV/ps2sdk
 export PATH=$PATH:$PS2DEV/bin
 export PATH=$PATH:$PS2DEV/ee/bin
 export PATH=$PATH:$PS2DEV/iop/bin
-export PATH=$PATH:$PS2DEV/dvp/bin
+# export PATH=$PATH:$PS2DEV/dvp/bin
 export PATH=$PATH:$PS2SDK/bin
 
 ## Run the toolchain script.
